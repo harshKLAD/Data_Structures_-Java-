0295e6df8081b6804dc39c3ea8a8345066e7f0d9
harshLAD <harshk.lad@gmail.com>
2020-11-22 15:52:39 -0500

    Added Linked List

---
+++ Singly Linked List/src/Harsh/Main.java
@@ -0,0 +1,25 @@
+package Harsh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        linkedList Linked_list = new linkedList();
+
+        Node A = new Node();
+	    A.value = 20;
+		Node B = new Node();
+		B.value = 32423;
+		Node C = new Node();
+		C.value = 54;
+		Node D = new Node();
+		D.value = 996;
+
+	    Linked_list.add(A);
+		Linked_list.add(B);
+		Linked_list.add(C);
+		Linked_list.add(D);
+
+		Linked_list.printList();
+		//System.out.println(Linked_list.head);
+    }
+}
